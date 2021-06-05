01c24a08d7e6aa91f89c8ab32c1f31fea7765f37
Shin Maung Maung <ethereal97@gmail.com>
2021-06-06 03:15:07 +0630

    undefined

--- live-stream.js
+++ live-stream.js
@@ -3,10 +3,10 @@ const createLiveStream = require('./src/createLiveStream');
 const updateLiveStream = require('./src/updateLiveStream');
 
 const { exec } = require('shelljs');
-const { default: axios } = require('axios');
-const { FACEBOOK_GRAPH_URL, FACEBOOK_PAGE_TOKEN } = require('./config');
 
-getVideoInfo('https://www.youtube.com/watch?v=WiwfiVdfRIc')
+let youtube_id = process.argv.slice(2)[0];
+
+getVideoInfo(`https://www.youtube.com/watch?v=${youtube_id}`)
   .then(async ({ title, channelName, content, formats }) => {
     let format = formats[0];
     if (!format) throw "no video source is available.";
@@ -15,7 +15,7 @@ getVideoInfo('https://www.youtube.com/watch?v=WiwfiVdfRIc')
       description: content,
     });
     let { video_id } = await updateLiveStream(id);
-    exec(`ffmpeg -re -i '${format.url}' -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv '${stream_url}'`);
+    exec(`ffmpeg -re -i '${format.url}' -c:v libx264 -preset fast -tune zerolatency -c:a aac -ar 44100 -f flv '${stream_url}'`);
   }).catch(e => {
     console.log(e.response?.headers, e.response?.data);
     console.log(e.message);
