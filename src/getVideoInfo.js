257360eeb911e2edc9a6c76ce02dc8fdad95605a
Shin Maung Maung <ethereal97@gmail.com>
2021-06-06 02:51:48 +0630

    undefined

--- src/getVideoInfo.js
+++ src/getVideoInfo.js
@@ -3,23 +3,23 @@ const toUnicode = require('./toUnicode');
 const createContentForFacebook = require('./createContentForFacebook');
 
 async function getVideoInfo(youtube_url) {
-    let { videoDetails, formats } = await ytdl.getBasicInfo(youtube_url);
+    let { videoDetails, formats } = await ytdl.getInfo(youtube_url);
     let data = {
         title: toUnicode(videoDetails.title),
         description: toUnicode(videoDetails.description),
         thumbnail: videoDetails.thumbnails.pop().url,
         channelName: videoDetails.ownerChannelName,
         url: videoDetails.video_url,
-        formats: [],
         content: createContentForFacebook(videoDetails),
+        formats: [],
     };
-
+    formats = formats.sort((a, b) => b.bitrate - a.bitrate);
     data.formats = formats.filter(({
         container,
         hasAudio,
         hasVideo
-    }) => hasAudio && hasVideo && container === 'mp4' || container === 'ts');
-
+    }) => hasAudio && hasVideo && ['ts', 'mp4'].includes(container));
+    console.log(data.formats);
     return data;
 }
 
