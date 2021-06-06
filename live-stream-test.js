7d9d789bfb567fda7ac7f295ba0c6f778fff1889
Shin Maung Maung <ethereal97@gmail.com>
2021-06-06 15:18:57 +0630

    undefined

---
+++ live-stream-test.js
@@ -0,0 +1,33 @@
+const getVideoInfo = require('./src/getVideoInfo');
+const createLiveStream = require('./src/createLiveStream');
+const updateLiveStream = require('./src/updateLiveStream');
+const deleteLiveStream = require('./src/deleteLiveStream');
+
+const { exec } = require('child_process');
+const input = process.argv[3];
+const url = input.length === '11' ? `https://www.youtube.com/watch?v=${input}` : input;
+
+getVideoInfo(url)
+  .then(async ({ title, channelName, content, formats }) => {
+    let format = formats[0];
+    if (!format) throw "no video source is available.";
+    let { id, stream_url } = await createLiveStream({
+      title: `${title} - ${channelName}`,
+      description: content,
+    });
+    let { video_id } = await updateLiveStream(id);
+    let cmd = [
+      `ffmpeg -re -i '${format.url}' -deinterlace -vcodec libx264`,
+      '-pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 2500k',
+      '-acodec libfdk_aac -b:a 128k -ar 44100 -threads 6 -qscale 3 -b:a',
+      `712000 -bufsize 512k -f flv '${stream_url}'`,
+    ].join(' ');
+    exec(cmd).on('error', async (err) => {
+      console.error(err);
+      console.error('Failed to stream live!', id);
+      await deleteLiveStream(id);
+    });
+  }).catch(e => {
+    console.log(e.response?.headers, e.response?.data);
+    console.log(e.message);
+  });
