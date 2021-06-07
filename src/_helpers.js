b267d54392555b0a15b246de7c81a34419f208ef
Shin Maung Maung <ethereal97@gmail.com>
2021-06-08 05:46:43 +0630

    fixed lag on live stream

--- src/_helpers.js
+++ src/_helpers.js
@@ -3,7 +3,7 @@ const { exec: execBG } = require('child_process');
 
 function saveLiveStream(input, output) {
     console.log('[1/2] saving live streaming video to local file at %s', output);
-    return execBG(`ffmpeg -i '${input}' -c:v libx264 -b:v 5M -bufsize 5M -maxrate 5M -threads 32 -g 120 -r 60 -c:a aac -ar 44100 '${output}' -y`)
+    return execBG(`ffmpeg -re -i '${input}' '${output}' -y`)
 }
 
 function broadcastLiveStream(input, output) {
@@ -11,10 +11,10 @@ function broadcastLiveStream(input, output) {
     return exec(`ffmpeg -re -i '${input}' -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -f flv '${output}'`)
 }
 
-function optimizeLiveStream(source_url, path, stream_url, timeout = 10000) {
+function optimizeLiveStream(source_url, path, stream_url, timeout = 30000) {
     saveLiveStream(source_url, path)
     setTimeout(() => {
-        broadcastLiveStream(path, stream_url)
+        broadcastLiveStream(path, stream_url + 3820)
     }, timeout)
 }
 
