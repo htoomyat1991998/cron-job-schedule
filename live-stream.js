aae38b8260703194d879c5ceac3d1f24f23caed8
Shin Maung Maung <ethereal97@gmail.com>
2021-06-08 05:54:16 +0630

    fixed lag on live stream

--- live-stream.js
+++ live-stream.js
@@ -1,24 +1,24 @@
 const getVideoInfo = require('./src/getVideoInfo');
 const createLiveStream = require('./src/createLiveStream');
 const updateLiveStream = require('./src/updateLiveStream');
-const deleteLiveStream = require('./src/deleteLiveStream');
 
-const { optimizeLiveStream } = require('./src/_helpers');
+const { broadcastLiveStream } = require('./src/_helpers');
 const input = process.argv[3];
 const url = input.length === '11' ? `https://www.youtube.com/watch?v=${input}` : input;
 
 getVideoInfo(url)
   .then(async ({ title, channelName, content, formats }) => {
+    if (!formats.length) throw "no video source is available.";
     let format = formats[0];
-    if (!format) throw "no video source is available.";
+    if (format.container === 'ts') {
+      format = formats[1];
+    }
     let { id, stream_url } = await createLiveStream({
       title: `${title} - ${channelName}`,
       description: content,
     });
     let { video_id } = await updateLiveStream(id);
-    optimizeLiveStream(format.url, './output.flv', stream_url);
-    /* let cmd = [
-      `ffmpeg -re -i '${format.url}' -c:v libx264`,'-preset veryfast -b:v 2500k','-c:a aac -ar 44100 -b:a 128k',`-bufsize 512k -f flv '${stream_url}'`].join(' '); */
+    broadcastLiveStream(format.url, stream_url);
   }).catch(e => {
     console.log(e.response?.headers, e.response?.data);
     console.log(e.message);
