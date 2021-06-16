f1bb87f38bfac1d200871880a41c25f718d62fd4
Shin Maung Maung <ethereal97@gmail.com>
2021-06-16 13:17:42 +0630

    cron schedule workflow

--- cron-test.js
+++ cron-test.js
@@ -1,17 +1,8 @@
 const cron = require('node-cron');
-const broadcastLiveStreamFromYoutube = require('./src/broadcastLiveStreamFromYoutube')
+const { exec } = require('shelljs');
+const [/* node */,/* __filename */, channel, cron_schedule, youtube_id] = process.argv;
 
-const [, , search, cron_schedule] = process.argv;
-const rerun = () => broadcastLiveStreamFromYoutube(search).catch((e) => rerun())
-
-// RFA (7PM)
 cron.schedule(cron_schedule, async () => {
-    try {
-        await rerun();
-    } catch (e) {
-        console.log(e);
-        await rerun();
-    }
-}, {
-    timezone: 'Asia/Rangoon'
-})
+    const command = `node ${__dirname}/live-stream.js '${channel}' '${youtube_id}'`;
+    exec(command);
+}, { timezone: 'Asia/Rangoon' });
