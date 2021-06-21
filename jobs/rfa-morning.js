8a70b7ec34348ddf6241951bfe3b802af41c1775
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-21 19:09:43 +0630

    Update rfa-morning.js

---
+++ jobs/rfa-morning.js
@@ -0,0 +1,39 @@
+const { schedule } = require("node-cron");
+const { searchUntilLiveOnYoutube, fetchUntilLiveFromYoutube, broadcastLiveStream } = require("../src/_helpers");
+const createLiveStream = require("../src/createLiveStream");
+const updateLiveStream = require("../src/updateLiveStream");
+const deleteLiveStream = require("../src/deleteLiveStream");
+
+const CRON_SCHEDULE = '0 0 7 * * *'; // 07:00:00.000
+const QUERY_STRING = 'RFA နေ့စဉ်တိုက်ရိုက်ထုတ်လွှင့်ချက်';
+
+schedule(CRON_SCHEDULE, () => start(), { timezone: 'Asia/Rangoon' });
+
+function start() {
+    let liveId;
+
+    searchUntilLiveOnYoutube(QUERY_STRING)
+        .then((videoId) => {
+            return fetchUntilLiveFromYoutube(videoId);
+        })
+        .then(async ({ title, channelName, content, formats }) => {
+            let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p');
+
+            let { id, stream_url } = await createLiveStream({
+                title: `${title} - ${channelName}`,
+                description: content,
+            });
+
+            liveId = id;
+
+            let { video_id } = await updateLiveStream(liveId);
+
+            broadcastLiveStream(format.url, stream_url);
+        })
+        .catch(err => {
+            console.error(err);
+            deleteLiveStream(liveId);
+            process.exit(1);
+        });
+}
+
