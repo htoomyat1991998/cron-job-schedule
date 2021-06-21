313a76b68adbec43c608c6b1d1749bb56b01fe31
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-22 04:35:38 +0630

    Update voa-morning.js

--- jobs/voa-morning.js
+++ jobs/voa-morning.js
@@ -2,29 +2,29 @@ const { schedule } = require("node-cron");
 const { searchUntilLiveOnYoutube, fetchUntilLiveFromYoutube, broadcastLiveStream } = require("../src/_helpers");
 const createLiveStream = require("../src/createLiveStream");
 const updateLiveStream = require("../src/updateLiveStream");
-
-const CRON_SCHEDULE = '4 0 6 * * *';
+const deleteLiveStream = require("../src/deleteLiveStream");
+                    /* s  m  h d  M D */
+const CRON_SCHEDULE = '4 0 6 * * *'; // at every 5:59:54 AM
 const QUERY_STRING = 'ဗီြအိုေအ ျမန္မာနံနက္ခင္း';
 
 schedule(CRON_SCHEDULE, start, { timezone: 'Asia/Rangoon' });
 
 function start() {
-    setTimeout(() => process.exit(), 3 * 60 * 60 * 1000); //max. 3hr
-
+    console.log('> querying "', QUERY_STRING, '" at', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+    
     searchUntilLiveOnYoutube(QUERY_STRING)
-        .then(({ videoId }) => {
-            return fetchUntilLiveFromYoutube(videoId);
-        })
+        .then(videoId => fetchUntilLiveFromYoutube(videoId))
         .then(async ({ title, channelName, content, formats }) => {
-            let format = formats[0];
-            if (format.container === 'ts') {
-                format = formats[1];
-            }
+            console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+            let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p');
             let { id, stream_url } = await createLiveStream({
                 title: `${title} - ${channelName}`,
                 description: content,
             });
             let { video_id } = await updateLiveStream(id);
-            broadcastLiveStream(format.url, stream_url);
+            setTimeout(() => console.log('EXIT') | process.exit(0), 3800 * 1000);
+            console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+            return broadcastLiveStream(format.url, stream_url);
         });
 }
+
