3d55631baba69e13b44961b07a6165d0caa10b03
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-22 05:57:49 +0630

    Update voa-morning.js

--- jobs/voa-morning.js
+++ jobs/voa-morning.js
@@ -4,7 +4,7 @@ const createLiveStream = require("../src/createLiveStream");
 const updateLiveStream = require("../src/updateLiveStream");
 const deleteLiveStream = require("../src/deleteLiveStream");
                     /* s  m  h d  M D */
-const CRON_SCHEDULE = '4 0 6 * * *'; // at every 5:59:54 AM
+const CRON_SCHEDULE = '54 59 5 * * *'; // at every 5:59:54 AM
 const QUERY_STRING = 'ဗီြအိုေအ ျမန္မာနံနက္ခင္း';
 
 schedule(CRON_SCHEDULE, start, { timezone: 'Asia/Rangoon' });
@@ -22,7 +22,6 @@ function start() {
                 description: content,
             });
             let { video_id } = await updateLiveStream(id);
-            setTimeout(() => console.log('EXIT') | process.exit(0), 3800 * 1000);
             console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
             return broadcastLiveStream(format.url, stream_url);
         });
